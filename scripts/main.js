57ba522821b5f8874dd9c954169839229623419e
Lina Parren <lina3@hotmail.nl>
2022-11-02 16:43:34 +0100

    added dataset

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,28 @@
-const h1 = document.querySelector('h1');
-const button = document.querySelector('button');
-const emojis = ['😃', '🙁', '🐭', '🍪', '🎱', '📊'];
+// const h1 = document.querySelector('h1');
+// const button = document.querySelector('button');
+// const emojis = ['😃', '🙁', '🐭', '🍪', '🎱', '📊'];
 
-button.addEventListener('click', pickEmoji);
+// button.addEventListener('click', pickEmoji);
 
-function pickEmoji() {
-	h1.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
-}
+// function pickEmoji() {
+// 	h1.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+// }
 
-pickEmoji();
+// pickEmoji();
 
+
+import myJson from '.tarieven.json' assert {type: 'json'};
+
+// 👇️ {
+//   name: 'Alice',
+//   country: 'Austria',
+//   tasks: [ 'develop', 'design', 'test' ],
+//   age: 30
+// }
+console.log(myJson.tarieven);
+
+// console.log(myJson.person.name); // 👉️ "Alice"
+// console.log(myJson.person.country); // 👉️ "Austria"
+
+console.log(myJson.description); 
+console.log(myJson.location.type);
