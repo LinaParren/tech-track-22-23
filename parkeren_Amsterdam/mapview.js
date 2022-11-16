17be507e1f275824d689e02962da14b510eb4954
Lina Parren <lina3@hotmail.nl>
2022-11-16 21:16:49 +0100

    tests with checkbox

--- parkeren_Amsterdam/mapview.js
+++ parkeren_Amsterdam/mapview.js
@@ -5,7 +5,7 @@ import json from "./tarieven.json" assert { type: "json" };
 
 
 
-var map = L.map('map').setView([52.3702157, 4.8951679], 14);
+var map = L.map('map').setView([52.3702157, 4.8951679], 12);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -39,49 +39,109 @@ console.log(Object.keys(json));
 // var polygon = L.polygon(json['T11A'].location.coordinates[0].flat().map(item => item.reverse()), {color: 'black'}).addTo(map);
 
 // ------ COMMENT DIT UIT!!!!!
-var polygon1 = L.polygon(json['T11A'].location.coordinates[0].map(item => item.reverse()), {color: 'black'}).addTo(map);
-var polygon2 = L.polygon(json['T11N'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
-var polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
-var polygon22 = L.polygon(json['T12A'].location.coordinates[0][1].map(item => item.reverse()), {color: 'blue'}).addTo(map);
-var polygon23 = L.polygon(json['T12A'].location.coordinates[0][2].map(item => item.reverse()), {color: 'blue'}).addTo(map);
-var polygon24 = L.polygon(json['T12A'].location.coordinates[0][3].map(item => item.reverse()), {color: 'blue'}).addTo(map);
-var polygon25 = L.polygon(json['T12A'].location.coordinates[1][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
-var polygon26 = L.polygon(json['T12A'].location.coordinates[2][0].map(item => item.reverse()), {color: 'red'}).addTo(map);
-var polygon27 = L.polygon(json['T12A'].location.coordinates[2][1].map(item => item.reverse()), {color: 'red'}).addTo(map);
-var polygon28 = L.polygon(json['T12A'].location.coordinates[2][2].map(item => item.reverse()), {color: 'red'}).addTo(map);
-var polygon29 = L.polygon(json['T12A'].location.coordinates[2][3].map(item => item.reverse()), {color: 'red'}).addTo(map);
-var polygon30 = L.polygon(json['T12A'].location.coordinates[3][0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
-var polygon31 = L.polygon(json['T12A'].location.coordinates[4][0].map(item => item.reverse()), {color: 'pink'}).addTo(map);
-var polygon32 = L.polygon(json['T12A'].location.coordinates[5][0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
-var polygon33 = L.polygon(json['T12A'].location.coordinates[6][0].map(item => item.reverse()), {color: 'grey'}).addTo(map);
-var polygon34 = L.polygon(json['T12A'].location.coordinates[7][0].map(item => item.reverse()), {color: 'gold'}).addTo(map);
-var polygon35 = L.polygon(json['T12A'].location.coordinates[8][0].map(item => item.reverse()), {color: 'white'}).addTo(map);
-var polygon36 = L.polygon(json['T12A'].location.coordinates[9][0].map(item => item.reverse()), {color: 'aqua'}).addTo(map);
+var polygon11 = L.polygon(json['T11N'].location.coordinates[0].map(item => item.reverse()), {color: 'red'}).addTo(map);
+var polygon12 = L.polygon(json['T11A'].location.coordinates[0].map(item => item.reverse()), {color: 'red'}).addTo(map);
+var polygon21 = L.polygon(json['T12A'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
+var polygon22 = L.polygon(json['T12B'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
+var polygon23 = L.polygon(json['T12C'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
+var polygon24 = L.polygon(json['T12D'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
+var polygon31 = L.polygon(json['T13A'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+var polygon32 = L.polygon(json['T13B'].location.coordinates[0][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+var polygon33 = L.polygon(json['T13C'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+var polygon34 = L.polygon(json['T13D'].location.coordinates[0][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+var polygon35 = L.polygon(json['T13G'].location.coordinates[0][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+var polygon41 = L.polygon(json['T14B'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map);
+var polygon42 = L.polygon(json['T14C'].location.coordinates[0][0].map(item => item.reverse()), {color: 'green'}).addTo(map);
+var polygon43 = L.polygon(json['T14E'].location.coordinates[0][0].map(item => item.reverse()), {color: 'green'}).addTo(map);
+var polygon44 = L.polygon(json['T14G'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map);
+var polygon51 = L.polygon(json['T16A'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+var polygon52 = L.polygon(json['T16B'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+var polygon53 = L.polygon(json['T16C'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+var polygon54 = L.polygon(json['T16D'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+var polygon61 = L.polygon(json['T17A'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+var polygon62 = L.polygon(json['T17C'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+var polygon63 = L.polygon(json['T17D'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+var polygon64 = L.polygon(json['T17E'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+var polygon65 = L.polygon(json['T17F'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+var polygon71 = L.polygon(json['T13B_U21'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+var polygon72 = L.polygon(json['T13B_U22'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+var polygon73 = L.polygon(json['T14B_U21'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+var polygon74 = L.polygon(json['T14B_U22'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+var polygon75 = L.polygon(json['T14B_U23'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+var polygon76 = L.polygon(json['T14B_U25'].location.coordinates[0][0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+
+// Verdeling prijzen en zones:
+// 1 7,50 -> T11N + T11A
+// 2 6,00 -> T12A, T12B, T12C, T12D
+// 3 4,50 -> T13A, T13B, T13C, T13D, T13G
+// 4 3,50 -> T14B, T14C, T14E, T14G
+// 5 2,50 -> T16A, T16B, T16C, T16D
+// 6 1,40 -> T17A, T17C, T17D, T17E, T17F
+// 7 0,10 -> T13B_U21, T13B_U22, T14B_U21, T14B_U22, T14B_U23, T14B_U25
 // -----------
 
-// -----------
-// const polygons = [
-// polygon1 = L.polygon(json['T11A'].location.coordinates[0].map(item => item.reverse()), {color: 'black'}).addTo(map),
-// polygon2 = L.polygon(json['T11N'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
-// polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'blue'}).addTo(map),
-// polygon22 = L.polygon(json['T12A'].location.coordinates[0][1].map(item => item.reverse()), {color: 'blue'}).addTo(map),
-// polygon23 = L.polygon(json['T12A'].location.coordinates[0][2].map(item => item.reverse()), {color: 'blue'}).addTo(map),
-// polygon24 = L.polygon(json['T12A'].location.coordinates[0][3].map(item => item.reverse()), {color: 'blue'}).addTo(map),
-// polygon25 = L.polygon(json['T12A'].location.coordinates[1][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map),
-// polygon26 = L.polygon(json['T12A'].location.coordinates[2][0].map(item => item.reverse()), {color: 'red'}).addTo(map),
-// polygon27 = L.polygon(json['T12A'].location.coordinates[2][1].map(item => item.reverse()), {color: 'red'}).addTo(map),
-// polygon28 = L.polygon(json['T12A'].location.coordinates[2][2].map(item => item.reverse()), {color: 'red'}).addTo(map),
-// polygon29 = L.polygon(json['T12A'].location.coordinates[2][3].map(item => item.reverse()), {color: 'red'}).addTo(map),
-// polygon30 = L.polygon(json['T12A'].location.coordinates[3][0].map(item => item.reverse()), {color: 'orange'}).addTo(map),
-// polygon31 = L.polygon(json['T12A'].location.coordinates[4][0].map(item => item.reverse()), {color: 'pink'}).addTo(map),
-// polygon32 = L.polygon(json['T12A'].location.coordinates[5][0].map(item => item.reverse()), {color: 'purple'}).addTo(map),
-// polygon33 = L.polygon(json['T12A'].location.coordinates[6][0].map(item => item.reverse()), {color: 'grey'}).addTo(map),
-// polygon34 = L.polygon(json['T12A'].location.coordinates[7][0].map(item => item.reverse()), {color: 'gold'}).addTo(map),
-// polygon35 = L.polygon(json['T12A'].location.coordinates[8][0].map(item => item.reverse()), {color: 'white'}).addTo(map),
-// polygon36 = L.polygon(json['T12A'].location.coordinates[9][0].map(item => item.reverse()), {color: 'aqua'}).addTo(map)
-// ];
 
-// export {polygons};
+// -----------
+// var polygon1 = L.polygon(json['T11A'].location.coordinates[0].map(item => item.reverse()), {color: 'black'}).addTo(map);
+// var polygon2 = L.polygon(json['T11N'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map);
+// var polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+// var polygon22 = L.polygon(json['T12A'].location.coordinates[0][1].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+// var polygon23 = L.polygon(json['T12A'].location.coordinates[0][2].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+// var polygon24 = L.polygon(json['T12A'].location.coordinates[0][3].map(item => item.reverse()), {color: 'blue'}).addTo(map);
+// var polygon25 = L.polygon(json['T12A'].location.coordinates[1][0].map(item => item.reverse()), {color: 'yellow'}).addTo(map);
+// var polygon26 = L.polygon(json['T12A'].location.coordinates[2][0].map(item => item.reverse()), {color: 'red'}).addTo(map);
+// var polygon27 = L.polygon(json['T12A'].location.coordinates[2][1].map(item => item.reverse()), {color: 'red'}).addTo(map);
+// var polygon28 = L.polygon(json['T12A'].location.coordinates[2][2].map(item => item.reverse()), {color: 'red'}).addTo(map);
+// var polygon29 = L.polygon(json['T12A'].location.coordinates[2][3].map(item => item.reverse()), {color: 'red'}).addTo(map);
+// var polygon30 = L.polygon(json['T12A'].location.coordinates[3][0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
+// var polygon31 = L.polygon(json['T12A'].location.coordinates[4][0].map(item => item.reverse()), {color: 'pink'}).addTo(map);
+// var polygon32 = L.polygon(json['T12A'].location.coordinates[5][0].map(item => item.reverse()), {color: 'purple'}).addTo(map);
+// var polygon33 = L.polygon(json['T12A'].location.coordinates[6][0].map(item => item.reverse()), {color: 'grey'}).addTo(map);
+// var polygon34 = L.polygon(json['T12A'].location.coordinates[7][0].map(item => item.reverse()), {color: 'gold'}).addTo(map);
+// var polygon35 = L.polygon(json['T12A'].location.coordinates[8][0].map(item => item.reverse()), {color: 'white'}).addTo(map);
+// var polygon36 = L.polygon(json['T12A'].location.coordinates[9][0].map(item => item.reverse()), {color: 'aqua'}).addTo(map);
+// // -----------
+
+
+
+// -----------
+// var polygon11 = [
+//     L.polygon(json['T11N'].location.coordinates[0].map(item => item.reverse()), {color: 'red'}).addTo(map),
+//     L.polygon(json['T11A'].location.coordinates[0].map(item => item.reverse()), {color: 'red'}).addTo(map)]
+// var polygon21 = [
+//     L.polygon(json['T12A'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map),
+//     L.polygon(json['T12B'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map),
+//     L.polygon(json['T12C'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map),
+//     L.polygon(json['T12D'].location.coordinates[0].map(item => item.reverse()), {color: 'orange'}).addTo(map)]
+// var polygon31 = [
+//     L.polygon(json['T13A'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map),
+//     L.polygon(json['T13B'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map),
+//     L.polygon(json['T13C'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map),
+//     L.polygon(json['T13D'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map),
+//     L.polygon(json['T13G'].location.coordinates[0].map(item => item.reverse()), {color: 'yellow'}).addTo(map)]
+// var polygon41 = [
+//     L.polygon(json['T14B'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map),
+//     L.polygon(json['T14C'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map),
+//     L.polygon(json['T14E'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map),
+//     L.polygon(json['T14G'].location.coordinates[0].map(item => item.reverse()), {color: 'green'}).addTo(map)]
+// var polygon51 = [
+//     L.polygon(json['T16A'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map),
+//     L.polygon(json['T16B'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map),
+//     L.polygon(json['T16C'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map),
+//     L.polygon(json['T16D'].location.coordinates[0].map(item => item.reverse()), {color: 'blue'}).addTo(map)]
+// var polygon61 = [
+//     L.polygon(json['T17A'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map),
+//     L.polygon(json['T17C'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map),
+//     L.polygon(json['T17D'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map),
+//     L.polygon(json['T17E'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map),
+//     L.polygon(json['T17F'].location.coordinates[0].map(item => item.reverse()), {color: 'purple'}).addTo(map)]
+// var polygon71 = [
+//     L.polygon(json['T13B_U21'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
+//     L.polygon(json['T13B_U22'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
+//     L.polygon(json['T14B_U21'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
+//     L.polygon(json['T14B_U22'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
+//     L.polygon(json['T14B_U23'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map),
+//     L.polygon(json['T14B_U25'].location.coordinates[0].map(item => item.reverse()), {color: 'brown'}).addTo(map)]
 // -----------
 
 
@@ -98,13 +158,45 @@ var polygon36 = L.polygon(json['T12A'].location.coordinates[9][0].map(item => it
 
 
 export default {
-    polygon1: polygon1,
-    polygon2: polygon2,
+    polygon11: polygon11,
+    polygon12: polygon12,
     polygon21: polygon21,
     polygon22: polygon22,
     polygon23: polygon23,
     polygon24: polygon24,
-    polygon25: polygon25,
-    polygon26: polygon26
+    polygon31: polygon31,
+    polygon32: polygon32,
+    polygon33: polygon33,
+    polygon34: polygon34,
+    polygon35: polygon35,
+    polygon41: polygon41,
+    polygon42: polygon42,
+    polygon43: polygon43,
+    polygon44: polygon44,
+    polygon51: polygon51,
+    polygon52: polygon52,
+    polygon53: polygon53,
+    polygon54: polygon54,
+    polygon61: polygon61,
+    polygon62: polygon62,
+    polygon63: polygon63,
+    polygon64: polygon64,
+    polygon65: polygon65,
+    polygon71: polygon71,
+    polygon72: polygon72,
+    polygon73: polygon73,
+    polygon74: polygon74,
+    polygon75: polygon75,
+    polygon76: polygon76,
 }
 
+
+// export default {
+//     polygon11: polygon11,
+//     polygon21: polygon21,
+//     polygon31: polygon31,
+//     polygon41: polygon41,
+//     polygon51: polygon51,
+//     polygon61: polygon61,
+//     polygon71: polygon71,
+// }
--- parkeren_Amsterdam/scripts/test.js
+++ parkeren_Amsterdam/scripts/test.js
@@ -4,41 +4,147 @@
 import polygons from "../mapview";
 
 console.log('dit is hier')
-console.log(polygons.polygon21);
+console.log(polygons.polygon1);
 
-// import "./main.js"
+// // ---------------------------------------------------------
 
-// import polygons from "../mapview.js";
+// let zone1 = document.querySelector('#zone1');
+// zone1.addEventListener('change', kleur);
 
-// console.log(polygons);
+// function kleur() {
+//     var check1 = document.getElementById("zone1");
+
+//     console.log(polygons.polygon4);
+
+//     if (check1.checked == true){
+//       polygons.polygon1.setStyle({color: 'red'});
+//     }
+//     else {
+//       polygons.polygon1.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone2 = document.querySelector('#zone2');
+// zone2.addEventListener('change', kleur2);
+
+// function kleur2() {
+//     var check2 = document.getElementById("zone2");
+
+//     console.log(polygons.polygon2);
+
+//     if (check2.checked == true){
+//       polygons.polygon2.setStyle({color: 'orange'});
+//     }
+//     else {
+//       polygons.polygon2.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone3 = document.querySelector('#zone3');
+// zone3.addEventListener('change', kleur3);
+
+// function kleur3() {
+//     var check3 = document.getElementById("zone3");
+
+//     console.log(polygons.polygon4);
+
+//     if (check3.checked == true){
+//       polygons.polygon3.setStyle({color: 'yellow'});
+//     }
+//     else {
+//       polygons.polygon3.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone4 = document.querySelector('#zone4');
+// zone4.addEventListener('change', kleur4);
+
+// function kleur4() {
+//     var check4 = document.getElementById("zone4");
+
+//     console.log(polygons.polygon4);
+
+//     if (check4.checked == true){
+//       polygons.polygon4.setStyle({color: 'green'});
+//     }
+//     else {
+//       polygons.polygon4.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone5 = document.querySelector('#zone5');
+// zone5.addEventListener('change', kleur5);
+
+// function kleur5() {
+//     var check5 = document.getElementById("zone5");
+
+//     console.log(polygons.polygon5);
+
+//     if (check5.checked == true){
+//       polygons.polygon5.setStyle({color: 'blue'});
+//     }
+//     else {
+//       polygons.polygon5.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone6 = document.querySelector('#zone6');
+// zone6.addEventListener('change', kleur6);
+
+// function kleur6() {
+//     var check6 = document.getElementById("zone6");
+
+//     console.log(polygons.polygon6);
+
+//     if (check6.checked == true){
+//       polygons.polygon6.setStyle({color: 'purple'});
+//     }
+//     else {
+//       polygons.polygon6.setStyle({color: 'none'});
+//     }
+// }
+
+// // ---------------------------------------------------------
+
+// let zone7 = document.querySelector('#zone7');
+// zone7.addEventListener('change', kleur7);
+
+// function kleur7() {
+//     var check7 = document.getElementById("zone7");
+
+//     console.log(polygons.polygon7);
+
+//     if (check7.checked == true){
+//       polygons.polygon7.setStyle({color: 'brown'});
+//     }
+//     else {
+//       polygons.polygon7.setStyle({color: 'none'});
+//     }
+// }
+
+// ---------------------------------------------------------
 
 
-let zone11 = document.querySelector('#zone1');
-zone11.addEventListener('change', kleur);
-
-function kleur() {
-    var checkB = document.getElementById("zone1");
-    var tekst1 = document.getElementById("tekst1");
-    // var pols = document.getElementById("pol21");
-    // var polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'blue'}).addTo(map);
-    
-
-    console.log(polygons.polygon21);
 
 
 
-    if (checkB.checked == true){
-      // polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'red'}).addTo(map);
-      tekst1.style.color = "green";
-      polygons.polygon21.setStyle({color: 'green'});
-    }
-    else {
-      // polygon21 = L.polygon(json['T12A'].location.coordinates[0][0].map(item => item.reverse()), {color: 'orange'}).addTo(map);
-      tekst1.style.color = "red";
-      polygons.polygon21.setStyle({color: 'red'});
-    }
-}
 
+
+
+
+
+
+// ---------------------------------------------------------
 
 let checkbox = document.querySelector('#myCheck');
 checkbox.addEventListener('click', changeCheckbox);
@@ -54,5 +160,124 @@ function changeCheckbox() {
     }
     else {
       text.style.color = "blue";
+    }
+}
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------
+
+let zone1 = document.querySelector('#zone1');
+zone1.addEventListener('change', kleur);
+
+function kleur() {
+    var check1 = document.getElementById("zone1");
+
+    if (check1.checked == true){
+      polygons.polygon11.setStyle({color: 'red'});
+    }
+    else {
+      polygons.polygon11.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone2 = document.querySelector('#zone2');
+zone2.addEventListener('change', kleur2);
+
+function kleur2() {
+    var check2 = document.getElementById("zone2");
+
+    if (check2.checked == true){
+      polygons.polygon21.setStyle({color: 'orange'});
+    }
+    else {
+      polygons.polygon21.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone3 = document.querySelector('#zone3');
+zone3.addEventListener('change', kleur3);
+
+function kleur3() {
+    var check3 = document.getElementById("zone3");
+
+    if (check3.checked == true){
+      polygons.polygon31.setStyle({color: 'yellow'});
+    }
+    else {
+      polygons.polygon31.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone4 = document.querySelector('#zone4');
+zone4.addEventListener('change', kleur4);
+
+function kleur4() {
+    var check4 = document.getElementById("zone4");
+
+    if (check4.checked == true){
+      polygons.polygon41.setStyle({color: 'green'});
+    }
+    else {
+      polygons.polygon41.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone5 = document.querySelector('#zone5');
+zone5.addEventListener('change', kleur5);
+
+function kleur5() {
+    var check5 = document.getElementById("zone5");
+
+    if (check5.checked == true){
+      polygons.polygon51.setStyle({color: 'blue'});
+    }
+    else {
+      polygons.polygon51.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone6 = document.querySelector('#zone6');
+zone6.addEventListener('change', kleur6);
+
+function kleur6() {
+    var check6 = document.getElementById("zone6");
+
+    if (check6.checked == true){
+      polygons.polygon61.setStyle({color: 'purple'});
+    }
+    else {
+      polygons.polygon61.setStyle({color: 'none'});
+    }
+}
+
+// ---------------------------------------------------------
+
+let zone7 = document.querySelector('#zone7');
+zone7.addEventListener('change', kleur7);
+
+function kleur7() {
+    var check7 = document.getElementById("zone7");
+
+    if (check7.checked == true){
+      polygons.polygon71.setStyle({color: 'brown'});
+    }
+    else {
+      polygons.polygon71.setStyle({color: 'none'});
     }
 }
